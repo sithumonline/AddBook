d2467a1c8611775b9ba8d64431f788b5f8485fbe
ebonynon <sithumsandeepap@gmail.com>
2020-09-15 02:32:30 +0530

    add remove book

---
+++ src/app/components/remove-book/remove-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-book',
+  templateUrl: './remove-book.component.html',
+  styleUrls: ['./remove-book.component.css']
+})
+export class RemoveBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
