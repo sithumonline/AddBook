1afdc37fd74f9d890b553523c7ec47e563870112
ebonynon <sithumsandeepap@gmail.com>
2020-09-14 15:45:34 +0530

    add books table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
+import { BooksTableComponent } from './components/books-table/books-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     CardComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    BooksTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/books-table/books-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books-table',
+  templateUrl: './books-table.component.html',
+  styleUrls: ['./books-table.component.css']
+})
+export class BooksTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
