49d5ffeab8cda8516dd4b40c45b121a5ff11e771
ebonynon <sithumsandeepap@gmail.com>
2020-09-14 09:06:51 +0530

    add heroes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardComponent
+    CardComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
