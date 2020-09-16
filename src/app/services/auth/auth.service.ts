85e6472cfc1a5a388f838d9f173e77f83678a0ee
ebonynon <sithumsandeepap@gmail.com>
2020-09-16 14:36:21 +0530

    srtup auth serv

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  API_URL: string = 'https://mern-01.now.sh/';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(email: string, password: string) {
+    return this.http.post<string>(
+      this.API_URL,
+      { email, password },
+      httpOptions
+    );
+  }
 }
