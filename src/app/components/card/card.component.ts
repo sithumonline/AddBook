72b3266a4b06af34ef61b3a2b223f65a72323fcd
ebonynon <sithumsandeepap@gmail.com>
2020-09-16 22:42:50 +0530

    cleanup card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,27 +8,9 @@ import * as faker from 'faker';
 })
 export class CardComponent implements OnInit {
   @Input() Book: any;
-  name: string = 'Card';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  bio: string = faker.lorem.sentence();
   img: string = faker.image.city();
 
-  constructor() {
-    this.changeName('Mick');
-  }
+  constructor() {}
 
   ngOnInit(): void {}
-
-  changeName(name: string): void {
-    this.name = name;
-  }
-
-  onKey(event: any) {
-    //this.name = event.target.value;
-    this.changeName(event.target.value);
-  }
-
-  addHero(newHero: string) {
-    this.heroes.push(newHero);
-  }
 }
