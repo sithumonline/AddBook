4bde4542cbebf840bfa001d6bc4ac6df2663deff
ebonynon <sithumsandeepap@gmail.com>
2020-09-16 12:13:41 +0530

    setup log comp

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  password: string;
+  username: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleLogin() {
+    console.log('Heee');
   }
-
 }
